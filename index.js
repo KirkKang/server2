147b3296bd71a9d397064cf506b4889bd9b32df8
KirkKang <x963842@gmail.com>
2025-03-04 01:14:30 +0800

    改變index.js

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001 ;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Heroku!");
-});
 
 app.listen(PORT, () => {
   console.log(`Yey, your server is running on port ${PORT}`);
